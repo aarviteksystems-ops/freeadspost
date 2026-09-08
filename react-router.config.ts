87f3eff838484bf8c2c7ef0a522965f360ff2fe4
aarviteksystems-ops <aarviteksystems@gmail.com>
2026-09-08 09:32:40 +0530

    data on sheet issue fix

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,15 +1,6 @@
-// import type { Config } from "@react-router/dev/config";
-
-// export default { 
-//   // Config options...
-//   // Server-side render by default, to enable SPA mode set this to `false`
-//   ssr: true,
-// } satisfies Config;
-
 import type { Config } from "@react-router/dev/config";
-import { vercelPreset } from "@vercel/react-router/vite";
 
 export default {
   ssr: true,
-  presets: [vercelPreset()],
 } satisfies Config;
+
